ced2f0df4566f8f31ad8599d732472854037bf64
Rafael Felix <rfelixg@gmail.com>
2016-05-01 15:31:13 -0700

    Beta 1

--- js/main.js
+++ js/main.js
@@ -19,3 +19,51 @@
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Add jQuery Ready Handler
+$(document).ready(function(){
+
+// Invoke the Leaflet.js library to draw a map in your '#map-container' div.
+var map = L.map('map-container').setView([46.852, -121.760], 13);
+
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+// Add 2 layers to your map you have visuals. Use the Open Street Maps tiles served through the MapQuest CDN.
+var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+var mapLayers = {
+    "Satellite": satLayer,
+    "Map View": drawLayer,
+    "Open Street Maps": osm
+}
+
+L.control.layers(mapLayers).addTo(map);
+satLayer.addTo(map);
+
+// Customize that Map to show markers with popups at no fewer than 3
+// interesting locations. (You'll need to figure out the latitude/longitude for
+// these locations using a mapping tool such as Google Maps.)
+var marker = L.marker([46.852, -121.760]).addTo(map);
+marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+
+var markerTwo = L.marker([46.853765, -121.760203]).addTo(map);
+markerTwo.bindPopup("<b>Columbia Crest, Mt. Rainier's Highest Peak</b><br>This peak is 14,411 feet high.");
+
+var markerThree = L.marker([46.852319, -121.760326]).addTo(map);
+markerThree.bindPopup("<b>Point Success Peak</b><br>This peak is 14,158 feet high");
+
+var markerFour = L.makrer([46.852317, -121.760323]).addTo(map);
+markerFour.bindPopup("<b>Liberty Cap Peak</b><br>This peak is 14,112 feet high.");
+
+});
